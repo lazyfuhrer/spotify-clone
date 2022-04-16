00ae3ae3bdc4072dd56124fa82199f1c79e6a7b3
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-04-16 08:01:02 +0530

    Using middleware to redirect users to the app

---
+++ pages/_middleware.js
@@ -0,0 +1,19 @@
+import {getToken} from 'next-auth/jwt';
+//import {NextApiRequest} from 'next';
+import {NextRequest, NextResponse} from 'next/server';
+
+export async function middleware(req) {
+    const token = await getToken({req, secret: process.env.JWT_SECRET});
+    const url = req.nextUrl.clone();
+
+    const { pathname } = req.nextUrl;
+
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
+
+    if (!token && pathname !== '/login') {
+        url.pathname = '/login'
+        return NextResponse.redirect(url);
+    }
+}
