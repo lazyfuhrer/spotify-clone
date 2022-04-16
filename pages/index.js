5eeb3468f6aca3d69d02274c21b9358b09d27821
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-04-16 08:43:51 +0530

    Index

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { getSession } from "next-auth/react";
+import Sidebar from "../components/Sidebar";
+import Player from "../components/Player";
+import Center from "../components/Center";
+
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        <Sidebar />
+        <Center />
+      </main>
+
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
